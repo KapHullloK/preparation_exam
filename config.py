266f46838d99b492c9ea11313618eaa821c42336
afro_0 <kirillov2136@gmail.com>
2023-05-26 19:02:10 +0300

    commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///my_app.db'
+    SQLALCHEMY_DATABASE_URI = "postgresql://exam:exam@localhost/exam"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
     JSON_AS_ASCII = False
