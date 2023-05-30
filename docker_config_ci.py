d7f128f3c4b5b4a81351ec69b4fa4740a310c287
afro_0 <kirillov2136@gmail.com>
2023-05-30 11:11:11 +0300

    commit

--- docker_config_ci.py
+++ docker_config_ci.py
@@ -1,1 +1,6 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
+    JSON_AS_ASCII = False
